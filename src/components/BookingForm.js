e771044d92c025ce3ebe956378f11b3a4ffc9fad
Can Gokhan Kiziloglu <membercgk@me.com>
2023-10-13 22:15:54 +0300

    react hook form + yup validation implemented

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -3,128 +3,276 @@ import React, { useState } from "react";
 import bkimg1 from "../assets/booking1.jpg";
 import bkimg2 from "../assets/booking2.jpg";
 import bkimg3 from "../assets/booking3.jpg";
-import PropTypes from "prop-types";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const yesterday = new Date(Date.now() - 86400000);
 
 const BookingForm = ({ availableTimes, dispatch, submit }) => {
-  // const [step, setStep] = useState(1);
-  const [seating, setSeating] = useState("");
-  const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
-  const [time, setTime] = useState("");
-  const [guests, setGuests] = useState("");
-  const [occasion, setOccasion] = useState("");
+  // yup Schema
+  const book1Schema = yup.object().shape({
+    seating: yup.string().required("Please select a seating option"),
+    date: yup
+      .date()
+      .min(yesterday, "Date cannot be in the past")
+      .required("Please select a date"),
+    time: yup
+      .string()
+      .required("Please select a time")
+      .notOneOf(["Choose Time"], "Please select a time"),
+    guests: yup
+      .number()
+      .integer()
+      .positive("Please select a number of guests")
+      .min(1, "You should input at lease 1 guest")
+      .max(8, "We can accomodate a maximum of 8 guests per table"),
+    occasion: yup
+      .string()
+      .required("Please select an occasion")
+      .notOneOf(["Occasion"], "Please select an occasion"),
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const book2Schema = yup.object().shape({
+    firstname: yup
+      .string()
+      .required("Please enter your first name")
+      .min(2, "Name should be at least 2 characters")
+      .max(30, "Name should be less than 30 characters"),
+    lastname: yup
+      .string()
+      .required("Please enter your last name")
+      .min(2, "Name should be at least 2 characters")
+      .max(30, "Name should be less than 30 characters"),
+    email: yup
+      .string()
+      .email("Invalid email address")
+      .required("Please enter your email address"),
+    requests: yup
+      .string()
+      .max(200, "Requests should be less than 200 characters"),
+    terms: yup
+      .boolean()
+      .oneOf([true], "You must agree to the terms and conditions"),
+  });
 
-    submit({
-      seating: seating,
-      date: date,
-      time: time,
-      guests: guests,
-      occasion: occasion,
-    });
+  const bookSchema = book1Schema.concat(book2Schema);
 
-    setSeating("");
-    setDate("");
-    setTime("");
-    setGuests("");
-    setOccasion("");
+  // React Hook Form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isValid },
+    getValues,
+    reset,
+  } = useForm({
+    resolver: yupResolver(bookSchema),
+    mode: "onTouched",
+    defaultValues: {
+      seating: "Indoor",
+      date: Date(),
+      time: "",
+      guests: 1,
+      occasion: "",
+      firstname: "",
+      lastname: "",
+      email: "",
+      requests: "",
+      terms: false,
+    },
+  });
+
+  const [step, setStep] = useState(1);
+
+  const bookSubmit = (data) => {
+    submit(data);
+    reset();
   };
 
   return (
     <div className="booking-wrapper">
       <div className="booking">
         <h1 className="book-title">Reservation</h1>
-        <form className="book-form" onSubmit={handleSubmit} id="booking-form">
-          <section className="reserve">
-            <label>
-              Indoor Seating{" "}
-              <input
-                name="seating"
-                type="radio"
-                onChange={(e) => setSeating(e.target.value)}
-                value="Indoor"
-              />
-            </label>
-            <label>
-              Outdoor Seating{" "}
-              <input
-                name="seating"
-                type="radio"
-                onChange={(e) => setSeating(e.target.value)}
-                value="Outdoor"
-              />
-            </label>
+        <form
+          className="book-form"
+          onSubmit={handleSubmit(bookSubmit)}
+          id="booking-form"
+        >
+          {step === 1 && (
+            <section className="reserve">
+              <label>
+                Indoor Seating{" "}
+                <input {...register("seating")} type="radio" value="Indoor" />
+              </label>
+              <label>
+                Outdoor Seating{" "}
+                <input {...register("seating")} type="radio" value="Outdoor" />
+              </label>
+              {errors.seating && (
+                <p className="error">{errors.seating.message}</p>
+              )}
 
-            <label>
-              Choose date{" "}
-              <input
-                type="date"
-                required
-                onChange={(e) => {
-                  dispatch({ date: e.target.value });
-                  setDate(e.target.value);
-                }}
-                value={date}
-              ></input>
-            </label>
+              <label>
+                Choose date{" "}
+                <input
+                  {...register("date")}
+                  type="date"
+                  onChange={(e) => {
+                    dispatch({ date: e.target.value });
+                  }}
+                ></input>
+                {errors.date && <p className="error">{errors.date.message}</p>}
+              </label>
 
-            <label>
-              Choose time
-              <select
-                required
-                onChange={(e) => setTime(e.target.value)}
-                value={time}
-              >
-                <option>Choose Time</option>
-                {availableTimes.map((avtime) => (
-                  <option key={avtime} value={avtime}>
-                    {avtime}
-                  </option>
-                ))}
-              </select>
-            </label>
+              <label>
+                Choose time
+                <select {...register("time")}>
+                  <option>Choose Time</option>
+                  {availableTimes.map((avtime) => (
+                    <option key={avtime} value={avtime}>
+                      {avtime}
+                    </option>
+                  ))}
+                </select>
+                {errors.time && <p className="error">{errors.time.message}</p>}
+              </label>
 
-            <label>
-              Number of guests
-              <input
-                type="number"
-                required
-                onChange={(e) => setGuests(e.target.value)}
-                value={guests}
-              ></input>
-            </label>
+              <label>
+                Number of guests
+                <input {...register("guests")} type="number"></input>
+                {errors.guests && (
+                  <p className="error">{errors.guests.message}</p>
+                )}
+              </label>
 
-            <label>
-              Occasion
-              <select
-                required
-                onChange={(e) => setOccasion(e.target.value)}
-                value={occasion}
-              >
-                <option>Occasion</option>
-                <option value="Birthday">Birthday</option>
-                <option value="Anniversary">Anniversary</option>
-                <option value="Engagement">Engagement</option>
-              </select>
-            </label>
-          </section>
+              <label>
+                Occasion
+                <select {...register("occasion")}>
+                  <option>Occasion</option>
+                  <option value="No occasion">No Special Occasion</option>
+                  <option value="Birthday">Birthday</option>
+                  <option value="Anniversary">Anniversary</option>
+                  <option value="Engagement">Engagement</option>
+                </select>
+                {errors.occasion && (
+                  <p className="error">{errors.occasion.message}</p>
+                )}
+              </label>
+            </section>
+          )}
+          {step === 2 && (
+            <section className="confirm">
+              <button className="back-btn" onClick={() => setStep(1)}>
+                <i className="fa-solid fa-backward-step fa-xl"></i>
+                Back to Reservation Details
+              </button>
+
+              <p className="confirm-text">
+                Please provide your information to confirm your reservation. Go
+                back to change reservation details.
+              </p>
+
+              <label>
+                First Name
+                <input {...register("firstname")} type="text"></input>
+                {errors.firstname && (
+                  <p className="error">{errors.firstname.message}</p>
+                )}
+              </label>
+
+              <label>
+                Last Name
+                <input {...register("lastname")} type="text"></input>
+                {errors.lastname && (
+                  <p className="error">{errors.lastname.message}</p>
+                )}
+              </label>
+
+              <div className="res-details">
+                <h3>Reservation Details</h3>
+                <p>
+                  <b>Date:</b> {getValues("date")}
+                </p>
+                <p>
+                  <b>Time:</b> {getValues("time")}{" "}
+                </p>
+                <p>
+                  <b>Guests:</b> {getValues("guests")}
+                </p>
+                <p>
+                  <b>Occasion:</b> {getValues("occasion")}
+                </p>
+                <p>
+                  <b>Seating:</b> {getValues("seating")}
+                </p>
+              </div>
+
+              <label>
+                Email
+                <input {...register("email")} type="email"></input>
+                {errors.email && (
+                  <p className="error">{errors.email.message}</p>
+                )}
+              </label>
+
+              <label className="requests">
+                Special Requests
+                <textarea
+                  rows={5}
+                  {...register("requests")}
+                  type="text"
+                ></textarea>
+                {errors.requests && (
+                  <p className="error">{errors.requests.message}</p>
+                )}
+              </label>
+
+              <label className="terms">
+                <input {...register("terms")} type="checkbox"></input>I agree to
+                the terms and conditions
+                {errors.terms && (
+                  <p className="error">{errors.terms.message}</p>
+                )}
+              </label>
+            </section>
+          )}
         </form>
         <div className="book-img">
           <img src={bkimg1} alt="inside the restaurant" />
           <img src={bkimg2} alt="chef preparing dish" />
           <img src={bkimg3} alt="prepared dish being served" />
         </div>
-        <button className="btn btn-reserve" type="submit" form="booking-form">
-          Reserve
-        </button>
+
+        {step === 1 && (
+          <button
+            className="btn btn-reserve"
+            onClick={() => {
+              const valid = book1Schema.isValidSync({
+                seating: getValues("seating"),
+                date: getValues("date"),
+                time: getValues("time"),
+                guests: getValues("guests"),
+                occasion: getValues("occasion"),
+              });
+              if (valid) setStep(2);
+            }}
+          >
+            Reserve
+          </button>
+        )}
+        {step === 2 && (
+          <button
+            className="btn btn-reserve"
+            type="submit"
+            form="booking-form"
+            disabled={isSubmitting || !isValid}
+          >
+            Confirm Reservation
+          </button>
+        )}
       </div>
     </div>
   );
-};
-
-BookingForm.PropTypes = {
-  availableTimes: PropTypes.array.isRequired,
 };
 
 export default BookingForm;
--- src/pages/ConfirmedBooking.js
+++ src/pages/ConfirmedBooking.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import bkimg1 from "../assets/booking1.jpg";
 
 const ConfirmedBooking = () => {
   return (
-    <>
-          <Header />
-          
-          <h1 className="confirm-title">Your Reservation is confirmed</h1>
-
-          <Footer />
-    </>
+    <div className="confirm-wrapper">
+      <Header />
+      <div className="confirm-img">
+        <h1 className="confirm-title">Thank You!</h1>
+        <p className="confirm-p">
+          Your reservation has been confirmed. We look forward to seeing you!
+        </p>
+        
+        <img src={bkimg1} alt="Little Lemon Restaurant" />
+      </div>
+      <Footer />
+    </div>
   );
 };
 
